1eb6dc2bf06f145ac9126845bbbebcf68fce2fd1
GMarsura <137362859+GMarsura@users.noreply.github.com>
2025-05-18 22:42:50 -0300

    create backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TeamsModule } from './teams/teams.module';
+import { GamesModule } from './games/games.module';
+
+@Module({
+  imports: [TeamsModule, GamesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
